62c40f89222e4454d80c5783e1e2ab21a17a92f4
brunohx <70305645+brunohx@users.noreply.github.com>
2022-08-24 20:30:32 -0300

    Update quiz.js

--- quiz.js
+++ quiz.js
@@ -25,7 +25,7 @@ $(document).ready(function(){
         var progress = 5;
     
         function validateQuiz() {
-            progress += 10;
+            progress += 10.3;
             $("#analyzeBar").css("width", Math.min(progress, 100) + "%");
             $("#analyzeText").html(Math.min(Math.round(progress), 100) + "%");
             if (progress >= 100) {
