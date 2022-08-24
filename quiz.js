5a33f65b694630711263f480d17fe529c029be7f
brunohx <70305645+brunohx@users.noreply.github.com>
2022-08-24 17:39:17 -0300

    Add files via upload

---
+++ quiz.js
@@ -0,0 +1,50 @@
+$(document).ready(function(){
+        
+        $('.bq1').click((e) => {
+            e.preventDefault();
+            $('.p1').fadeOut('slow', ()=> {
+                $('.p2').fadeIn('slow');
+            });
+        })
+        
+        $('.bq2').click((e) => {
+            e.preventDefault();
+            $('.p2').fadeOut('slow', ()=> {
+                $('.p3').fadeIn('slow');
+            });
+        })
+        
+        $('.bq3').click((e) => {
+            e.preventDefault();
+            $('.p3').fadeOut('slow', ()=> {
+                $('.validatingDiv').fadeIn('slow',validateQuiz());
+            });
+        })
+
+        
+        var progress = 5;
+    
+        function validateQuiz() {
+            progress += 3.7;
+            $("#analyzeBar").css("width", Math.min(progress, 100) + "%");
+            $("#analyzeText").html(Math.min(Math.round(progress), 100) + "%");
+            if (progress >= 100) {
+                progress = 100;
+                $("#analyzeBar").removeClass("progress-bar-striped");
+                $("#analyzeText").removeClass("text-right").addClass("text-center");
+                $("#analyzeText").html("Concluído!");
+                setTimeout(function () {
+                    $('.validatingDiv').fadeOut('slow', function () {
+                        $('.hp2').fadeIn('slow');
+                        $('.validatedDiv').fadeIn('slow');
+                    });
+                }, 2000);
+            }
+        
+            if (progress < 100) {
+                setTimeout(function () {
+                    validateQuiz();
+                }, 250);
+            }
+        }
+    });
